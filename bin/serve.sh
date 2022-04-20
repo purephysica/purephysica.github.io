6b95954ea5c40ba939996925de77c6b476218a23
Yukihiro Arisawa <iamyukihiro@pop-os.localdomain>
2022-04-20 21:56:29 +0900

    jekyllコンテナが落ちていたので、古いものを使うように

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --volume="$PWD:/srv/jekyll" -it -p 4000:4000 jekyll/jekyll:pages jekyll serve
+docker run --rm --volume="$PWD:/srv/jekyll" -it -p 4000:4000 jekyll/jekyll:4.1.0 jekyll serve
